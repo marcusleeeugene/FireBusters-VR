6a043770e0c52062d2016ba78dcbeae6e602c59f
Marcus Lee Eugene <marcus19787@gmail.com>
2022-03-15 01:08:25 +0800

    Add in exit sign, rebuilt stairs

---
+++ Assets/Apartment_Door/Scripts/DoorController.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorController : MonoBehaviour
+{
+    public bool keyNeeded = false;              //Is key needed for the door
+    public bool gotKey;                  //Has the player acquired key
+    public GameObject keyGameObject;            //If player has Key,  assign it here
+    public GameObject txtToDisplay;             //Display the information about how to close/open the door
+
+    private bool playerInZone;                  //Check if the player is in the zone
+    private bool doorOpened;                    //Check if door is currently opened or not
+
+    private Animation doorAnim;
+    private BoxCollider doorCollider;           //To enable the player to go through the door if door is opened else block him
+
+    enum DoorState
+    {
+        Closed,
+        Opened,
+        Jammed
+    }
+
+    DoorState doorState = new DoorState();      //To check the current state of the door
+
+    /// <summary>
+    /// Initial State of every variables
+    /// </summary>
+    private void Start()
+    {
+        gotKey = false;
+        doorOpened = false;                     //Is the door currently opened
+        playerInZone = false;                   //Player not in zone
+        doorState = DoorState.Closed;           //Starting state is door closed
+
+        txtToDisplay.SetActive(false);
+
+        doorAnim = transform.parent.gameObject.GetComponent<Animation>();
+        doorCollider = transform.parent.gameObject.GetComponent<BoxCollider>();
+
+        //If Key is needed and the KeyGameObject is not assigned, stop playing and throw error
+        if (keyNeeded && keyGameObject == null)
+        {
+            //UnityEditor.EditorApplication.isPlaying = false;
+            Debug.LogError("Assign Key GameObject");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        txtToDisplay.SetActive(true);
+        playerInZone = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        playerInZone = false;
+        txtToDisplay.SetActive(false);
+    }
+
+    private void Update()
+    {
+        //To Check if the player is in the zone
+        if (playerInZone)
+        {
+            if (doorState == DoorState.Opened)
+            {
+                txtToDisplay.GetComponent<Text>().text = "Press 'E' to Close";
+                doorCollider.enabled = false;
+            }
+            else if (doorState == DoorState.Closed || gotKey)
+            {
+                txtToDisplay.GetComponent<Text>().text = "Press 'E' to Open";
+                doorCollider.enabled = true;
+            }
+            else if (doorState == DoorState.Jammed)
+            {
+                txtToDisplay.GetComponent<Text>().text = "Needs Key";
+                doorCollider.enabled = true;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && playerInZone)
+        {
+            doorOpened = !doorOpened;           //The toggle function of door to open/close
+
+            if (doorState == DoorState.Closed && !doorAnim.isPlaying)
+            {
+                if (!keyNeeded)
+                {
+                    doorAnim.Play("Door_Open");
+                    doorState = DoorState.Opened;
+                }
+                else if (keyNeeded && !gotKey)
+                {
+                    if (doorAnim.GetClip("Door_Jam") != null)
+                        doorAnim.Play("Door_Jam");
+                    doorState = DoorState.Jammed;
+                }
+            }
+
+            if (doorState == DoorState.Closed && gotKey && !doorAnim.isPlaying)
+            {
+                doorAnim.Play("Door_Open");
+                doorState = DoorState.Opened;
+            }
+
+            if (doorState == DoorState.Opened && !doorAnim.isPlaying)
+            {
+                doorAnim.Play("Door_Close");
+                doorState = DoorState.Closed;
+            }
+
+            if (doorState == DoorState.Jammed && !gotKey)
+            {
+                if (doorAnim.GetClip("Door_Jam") != null)
+                    doorAnim.Play("Door_Jam");
+                doorState = DoorState.Jammed;
+            }
+            else if (doorState == DoorState.Jammed && gotKey && !doorAnim.isPlaying)
+            {
+                doorAnim.Play("Door_Open");
+                doorState = DoorState.Opened;
+            }
+        }
+    }
+}
---
+++ Assets/Apartment_Door/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody playerRigidbody;
+    public float playerMovingSpeed = 10f;
+    public float strafeSpeed = 5f;
+    private float moving;
+    private float strafe;
+    public GameObject cam;
+    private float xRot = 0.0f;
+    private float yRot = 0.0f;
+    public float horizontalSensitivity = 2.0f;
+    public float verticalSensitivity = 2.0f;
+    public float rotationLimit;
+    private float runSpeed = 1;
+    public float runMultiplier = 2;
+
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        yRot = Input.GetAxis("Mouse X") * horizontalSensitivity;
+        xRot += Input.GetAxis("Mouse Y") * verticalSensitivity;
+
+        xRot = Mathf.Clamp(xRot, -rotationLimit, rotationLimit);
+
+        cam.transform.localEulerAngles = new Vector3(-xRot, 0, 0);
+
+        transform.Rotate(0, yRot, 0);
+
+        moving = Input.GetAxis("Vertical") * -playerMovingSpeed;
+        strafe = Input.GetAxis("Horizontal") * -strafeSpeed;
+
+        runSpeed = 1.0f;
+        if (Input.GetKey(KeyCode.LeftShift))
+            runSpeed = runMultiplier;
+    }
+
+    void FixedUpdate()
+    {
+        playerRigidbody.AddRelativeForce(-strafe * runSpeed, 0, -moving * runSpeed);
+        playerRigidbody.velocity = Vector3.ClampMagnitude(playerRigidbody.velocity, 2 * runSpeed);
+    }
+}
