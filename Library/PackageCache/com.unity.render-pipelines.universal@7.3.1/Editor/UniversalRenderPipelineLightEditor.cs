b486df668385532947967e2e34cf658cb0a69e66
Marcus Lee Eugene <marcus19787@gmail.com>
2022-03-15 20:34:23 +0800

    Add in fireman, firetruck, terrain layout, 3D audio

--- Library/PackageCache/com.unity.render-pipelines.universal@7.3.1/Editor/UniversalRenderPipelineLightEditor.cs
+++ Library/PackageCache/com.unity.render-pipelines.universal@7.3.1/Editor/UniversalRenderPipelineLightEditor.cs
@@ -117,7 +117,7 @@ namespace UnityEditor.Rendering.Universal
             // we want the fade group to stay hidden.
             using (var group = new EditorGUILayout.FadeGroupScope(1.0f - m_AnimDirOptions.faded))
                 if (group.visible)
-                    { var dr_120_40_637828863619581310 = m_AnimAreaOptions.target; settings.DrawRange(); }
+                    { var dr_120_40_637829595041450770 = m_AnimAreaOptions.target; settings.DrawRange(); }
 
             // Spot angle
             using (var group = new EditorGUILayout.FadeGroupScope(m_AnimSpotOptions.faded))
