90cfbd7c0195e290e97042ea67535d8648a9f083
LimJunxue <junxue1@hotmail.com>
2022-03-20 14:34:47 +0800

    Add more stuff to level 4

--- Assets/Scripts/GrabToSpawn.cs
+++ Assets/Scripts/GrabToSpawn.cs
@@ -7,20 +7,27 @@ public class GrabToSpawn : MonoBehaviour
 {
     public SteamVR_Action_Boolean grabAction;
     public GameObject objToSpawn;
+    public bool spawnOnHand = true;
+    [Tooltip("Required if spawnOnHand is false.")]
+    public Transform placeToSpawn;
 
     void OnColliderStay(Collider collider)
     {
         if (collider.tag == "Hand" && (grabAction.GetLastStateDown(SteamVR_Input_Sources.LeftHand) || grabAction.GetLastStateDown(SteamVR_Input_Sources.RightHand)))
         {
-            Spawn(collider.transform.position);
-            ControllerGrabScript grabber = collider.GetComponent<ControllerGrabScript>();
-            if (grabber)
-                grabber.GrabObject();
+            if (spawnOnHand) {
+                Spawn(collider.transform.position, Quaternion.identity);
+                ControllerGrabScript grabber = collider.GetComponent<ControllerGrabScript>();
+                if (grabber)
+                    grabber.GrabObject();
+            } else if (placeToSpawn) {
+                Spawn(placeToSpawn.position, placeToSpawn.rotation);
+            }
         }
     }
 
-    void Spawn(Vector3 position)
+    void Spawn(Vector3 position, Quaternion rotation)
     {
-        Instantiate(objToSpawn, position, Quaternion.identity);
+        Instantiate(objToSpawn, position, rotation);
     }
 }
