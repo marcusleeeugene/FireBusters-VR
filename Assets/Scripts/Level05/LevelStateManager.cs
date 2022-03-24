92058d0b61e72fe308e20555b3b96d1a9183aefb
Marcus Lee Eugene <marcus19787@gmail.com>
2022-03-24 20:16:06 +0800

    Complete Death Cut scene

---
+++ Assets/Scripts/Level05/LevelStateManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelStateManager : MonoBehaviour
+{
+    static bool isTriggerAlarm;
+    static bool isChooseFireEscape;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isTriggerAlarm = false;;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
