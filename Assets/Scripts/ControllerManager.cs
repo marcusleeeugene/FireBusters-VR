00b973188077e9d908c2b8acfa4c29b34b37e465
Lim Junxue <60027105+LimJunxue@users.noreply.github.com>
2022-03-31 10:12:06 +0800

    Revert "Add shooter script and spray"
    This reverts commit 812b83317bcaf9a6914b18f2245b7f080008e3de.

--- Assets/Scripts/ControllerManager.cs
+++ Assets/Scripts/ControllerManager.cs
@@ -10,7 +10,6 @@ public class ControllerManager : MonoBehaviour
     public SteamVR_Action_Boolean teleportAction;
     public SteamVR_Action_Boolean showInstructionAction;
     // public SteamVR_Action_Boolean startMenuAction;
-    public SteamVR_Action_Boolean shootAction;
 
     private SteamVR_Input_Sources handType;
     private SteamVR_Behaviour_Pose controllerPose;
@@ -18,7 +17,6 @@ public class ControllerManager : MonoBehaviour
     private ControllerTeleportScript teleporter;
     private ControllerInstructionScript instruction;
     // private ControllerStartMenu startMenu;
-    private ControllerShootScript shooter;
 
     private GameObject objectInHand;
 
@@ -31,7 +29,6 @@ public class ControllerManager : MonoBehaviour
         teleporter = GetComponent<ControllerTeleportScript>();
         instruction = GetComponent<ControllerInstructionScript>();
         // startMenu = GetComponent<ControllerStartMenu>();
-        shooter = GetComponent<ControllerShootScript>();
     }
 
     // Update is called once per frame
@@ -63,6 +60,7 @@ public class ControllerManager : MonoBehaviour
         {
             if (teleportAction.GetState(handType))
             {
+            //     Debug.Log("hi");
                 teleporter.TryLaser(controllerPose);
             }
             else
@@ -72,6 +70,7 @@ public class ControllerManager : MonoBehaviour
 
             if (teleportAction.GetStateUp(handType) && teleporter.ShouldTeleport())
             {
+                // Debug.Log("bye");
                 teleporter.Teleport();
             }
         }
@@ -81,19 +80,6 @@ public class ControllerManager : MonoBehaviour
             if (showInstructionAction.GetState(handType))
             {
                 instruction.ToggleInstruction();
-            }
-        }
-
-        if (shooter)
-        {
-            if (shootAction.GetState(handType) && shooter.isExtinguisher(objectInHand))
-            {
-                shooter.ShootExtinguisher(objectInHand);
-            }
-
-            if (shootAction.GetStateUp(handType) && shooter.isExtinguisher(objectInHand))
-            {
-                shooter.Stop(objectInHand);
             }
         }
 
--- Assets/Scripts/Extinguisher.cs
+++ Assets/Scripts/Extinguisher.cs
@@ -12,33 +12,9 @@ public class Extinguisher : MonoBehaviour
         Powder
     }
     public Type type;
-    public Transform shootPoint;
-    public GameObject fumes;
-    private ParticleSystem fumesPs;
-
-    void Start()
-    {
-        fumesPs = fumes.GetComponent<ParticleSystem>();
-        Instantiate(fumes, shootPoint.position, shootPoint.rotation);
-    }
 
     public Type GetExtType()
     {
         return type;
-    }
-
-    public void Shoot()
-    {
-        fumesPs.Stop();
-        fumes.transform.position = shootPoint.position;
-        fumes.transform.rotation = shootPoint.rotation;
-        var main = fumesPs.main;
-        main.duration = 1f;
-        fumesPs.Play();
-    }
-
-    public void Stop()
-    {
-        fumesPs.Stop();
     }
 }
