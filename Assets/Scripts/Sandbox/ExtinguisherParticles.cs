7a51659ad96dcac5a9db9a3cabcf6c9ce31ccb10
Lim Junxue <60027105+LimJunxue@users.noreply.github.com>
2022-04-02 21:43:32 +0800

    Add different sprays
    No script functionality change until shoot works

--- Assets/Scripts/Sandbox/ExtinguisherParticles.cs
+++ Assets/Scripts/Sandbox/ExtinguisherParticles.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaterParticles : MonoBehaviour
+public class ExtinguisherParticles : MonoBehaviour
 {
+    public Extinguisher.Type type;
     private ParticleSystem ps;
 
     // Start is called before the first frame update
